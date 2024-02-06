dc46eec26e1d2478ee6144d97bb567820af6ca9b
Black River <anblackriver@gmail.com>
2024-02-06 23:57:57 +0500

    App v1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <AppHeader/>
+
+  <AppFilers/>
+
+  <main class="app-main">
+    <AppTodoList/>
+
+    <AppAddTodo/>
+  </main>
+
+  <AppFooter/>
+</template>
+
+<script setup lang="ts">
+
+import AppHeader from "@/components/AppHeader.vue";
+import AppFilers from "@/components/AppFilers.vue";
+import AppTodoList from "@/components/AppTodoList.vue";
+import AppAddTodo from "@/components/AppAddTodo.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+</script>
